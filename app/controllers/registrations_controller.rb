5476a447f2f71bfc6abe16629eea5511a4c53f5e
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-22 16:55:36 -0400

    Fix interests path after sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    new_interest_path
+    new_user_interest_path(resource)
   end
 end
