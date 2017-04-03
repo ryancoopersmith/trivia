987a6dbc69fd8b60824f2ecd5b2d969daf936ddf
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-04-03 07:52:42 -0400

    Email users that have not played in more than a week a reminder

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -1,9 +1,13 @@
+require 'date'
+
 class EmailWorker
   include Sidekiq::Worker
 
   def perform
     User.all.each do |user|
-      UserMailer.reminder(user).deliver
+      if Time.at(user.updated_at + (7*24*60*60)).to_date <= Time.at(Time.now).to_date
+        UserMailer.reminder(user).deliver
+      end
     end
   end
 end
